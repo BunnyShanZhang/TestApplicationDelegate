32d42fc5e05180bd7202006fa14d95b5de778531
ZhangShan <13718370732@163.com>
2016-06-15 12:08:32 +0800

    Initial Commit

---
+++ TestApplicationDelegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestApplicationDelegate
+//
+//  Created by ZhangShan on 16/6/15.
+//  Copyright © 2016年 ZhangShan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
